649c2f349bfc75d0edb324a5cb5051e79f11c7d6
Steven HH Chen <s335276@gmail.com>
2021-04-08 15:09:24 +0800

    feat: add trader.py as main script

---
+++ trader.py
@@ -0,0 +1,42 @@
+# Trader - entry of this homework
+# This code is written by Steven HH Chen.
+
+# Python internal module import
+import argparse
+
+# Python external module import
+import pandas as pd
+from loguru import logger
+
+# User module import
+from utils import dataIO
+
+if __name__ == "__main__":
+
+    logger.success("  DSAI 2021 Spring Homework 02 - AutoTrading  ")
+    logger.success("<-- This code is written By Steven HH Chen -->")
+
+    print("-----------------------------------------------------------------------------------------------------------------------")
+
+    logger.info("Reading input arguments...")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--training',
+                        default='training.csv',
+                        help='input training data file name')
+    parser.add_argument('--testing',
+                        default='testing.csv',
+                        help='input testing data file name')
+    parser.add_argument('--output',
+                        default='output.csv',
+                        help='output file name')
+
+    args = parser.parse_args()
+    logger.debug(f"Training data input csv file    -> ./datasets/{args.training}")
+    logger.debug(f"Testing data input csv file     -> ./datasets/{args.testing}")
+    logger.debug(f"Model Prediction Outcome Output -> ./output/{args.output}")
+    
+    stockCol = ["Open", "High", "Low", "Close"]
+    stockIn = dataIO.read_csv_to_pandas_df(f"./datasets/{args.training}", stockCol)
+
+    print(stockIn)
